ef667bb46be576756f9cb14f4e6c86a24e29d4aa
hmmed1 <alnasriahmed.it@gmail.com>
2026-08-30 19:47:52 +0100

    Update README with manual parsing documentation

--- code/main.py
+++ code/main.py
@@ -1,5 +1,10 @@
 from scapy.all import sniff
 from packet_decoder import process
 
-
-sniff(prn=process, count=1)
+packet_list = []
+def collect_packet (packet):
+    packet_list.append(packet)
+    print("\n\n-------------------- Packet Number: "+str(len(packet_list))+"--------------------\n\n")
+    process(packet)
+sniff(prn=collect_packet, stop_filter=lambda p: len(packet_list) >= 10)
+print(len(packet_list))
